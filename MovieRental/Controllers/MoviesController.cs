0bacfe403f48e34de4006029cc3a720a92f7cc0f
Msluka <969179@gmail.com>
2017-05-19 23:21:25 +0200

    Custom Route Configuration, Constraints

--- MovieRental/Controllers/MoviesController.cs
+++ MovieRental/Controllers/MoviesController.cs
@@ -34,7 +34,12 @@ namespace MovieRental.Controllers
                 sortBy = "Name";
             }
 
-            return Content(String.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year +"/"+ month);
         }
 
     }
