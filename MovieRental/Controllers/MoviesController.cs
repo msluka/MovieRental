d07030c592727435703357b5cf6d72ec04591dec
Msluka <969179@gmail.com>
2017-05-19 22:20:15 +0200

    MovieRental

---
+++ MovieRental/Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRental.Models;
+
+namespace MovieRental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie {Name = "Shrek!"};
+            return View(movie);
+            
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
+        }
+
+    }
+}
