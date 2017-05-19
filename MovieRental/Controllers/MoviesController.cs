d5591967f21abe348de34a3ec918b28b3835a28f
Msluka <969179@gmail.com>
2017-05-20 00:01:35 +0200

    Route Attribute / routes.MapMvcAttributeRoutes()

--- MovieRental/Controllers/MoviesController.cs
+++ MovieRental/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using MovieRental.Models;
 
 namespace MovieRental.Controllers
@@ -37,6 +38,7 @@ namespace MovieRental.Controllers
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
 
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year +"/"+ month);
