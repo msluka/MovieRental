b68d2403c785ec9674f1922901c9cf0fe5feff8e
Msluka <969179@gmail.com>
2017-05-21 16:00:12 +0200

    Adding: Lists; Get and Details Methods

--- MovieRental/Controllers/MoviesController.cs
+++ MovieRental/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,24 +13,43 @@ namespace MovieRental.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
+
+        public ActionResult Index()
         {
-            var movie = new Movie {Name = "Shrek!"};
+            var movies = GetMovies();
+            return View(movies);
+        }
 
-            var customers = new List<Customer>()
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
             {
-                new Customer { Name = "Johne"},
-                new Customer { Name = "Anna"}
-
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"}
             };
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
+        }
 
-            return View(viewModel);
+
+
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie {Name = "Shrek!"};
+
+        //    var customers = new List<Customer>()
+        //    {
+        //        new Customer { Name = "Johne"},
+        //        new Customer { Name = "Anna"}
+
+        //    };
+
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
+
+        //    return View(viewModel);
 
             //ViewData["RandomMovieD"] = movie;
 
@@ -39,33 +59,33 @@ namespace MovieRental.Controllers
 
             //ViewBag.RandomMoviesB = "Shrak (string from ViewBag)";
 
-        }
+        //}
 
-        public ActionResult Edit(int id)
-        {
-            return Content("Id=" + id);
-        }
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("Id=" + id);
+        //}
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //    {
+        //        pageIndex = 1;
+        //    }
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //    {
+        //        sortBy = "Name";
+        //    }
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
 
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year +"/"+ month);
-        }
+        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year +"/"+ month);
+        //}
 
     }
 }
