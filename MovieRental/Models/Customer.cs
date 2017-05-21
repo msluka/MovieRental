aa538d7a08fd54df3c38469ddaeb7e7c9ee38026
Msluka <969179@gmail.com>
2017-05-21 20:10:10 +0200

    Changing the Model, Navigation Property, Foreign Key

--- MovieRental/Models/Customer.cs
+++ MovieRental/Models/Customer.cs
@@ -9,6 +9,9 @@ namespace MovieRental.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-               
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; } //Navigation property
+        public byte MembershipTypeId { get; set; } //Foreign Key
+        
     }
 }
