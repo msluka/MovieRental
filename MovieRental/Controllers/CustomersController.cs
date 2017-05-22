8377cafe32fcde91664c61c70a52ae1e848e183e
Msluka <969179@gmail.com>
2017-05-22 23:29:23 +0200

    Adding Customer Create Method

--- MovieRental/Controllers/CustomersController.cs
+++ MovieRental/Controllers/CustomersController.cs
@@ -49,6 +49,14 @@ namespace MovieRental.Controllers
             };
             return View(viewModel);
         }
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
 
         //private IEnumerable<Customer> GetCustomers()
         //{
