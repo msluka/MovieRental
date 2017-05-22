1e2f8d7896153c33656d9ea16cf0c6d89c307876
Msluka <969179@gmail.com>
2017-05-22 12:33:30 +0200

    Adding nullable Birthdate property % if statement in details view

--- MovieRental/Models/Customer.cs
+++ MovieRental/Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -12,6 +13,8 @@ namespace MovieRental.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        public DateTime? Birthdate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; } //Navigation property
         public byte MembershipTypeId { get; set; } //Foreign Key
