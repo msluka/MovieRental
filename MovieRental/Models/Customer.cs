474e1c985fd41197f77479b3ffa16385c811bdc0
Msluka <969179@gmail.com>
2017-05-22 16:14:46 +0200

    Different way to create Label and display Name

--- MovieRental/Models/Customer.cs
+++ MovieRental/Models/Customer.cs
@@ -13,7 +13,7 @@ namespace MovieRental.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; } //Navigation property
